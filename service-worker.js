33c850f92223a9832a173799c7e522e169aff43f
khuzwayo <vainesszulu07@gmail.com>
2025-10-18 02:26:27 +0200

    Update manifest, service worker, icons, and HTML for PWA

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "khuzwayos-cache-v2";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/movie/maly-192.png",
+  "/movie/maly-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
